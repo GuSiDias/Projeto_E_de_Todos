c1e1604ba202c618a3c4da751b2d48df15e80eab
GuSiDias <gusidias@gmail.com>
2023-09-01 10:05:02 -0300

    É de todos

---
+++ src/main/java/com/generation/EdeTodos/EdeTodosApplication.java
@@ -0,0 +1,13 @@
+package com.generation.EdeTodos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdeTodosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdeTodosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/EdeTodos/EdeTodosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.EdeTodos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdeTodosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
